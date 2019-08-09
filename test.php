4e6ad03e6fc2ae52ab26222e6bda659c31fc4cb3
gagan <gagandureja675@gmail.com>
2019-08-10 01:44:17 +0530

    Select problem solved

--- test.php
+++ test.php
@@ -1,13 +1,40 @@
 <?php 
 $connect = mysqli_connect('localhost', 'root', 'Password@123', 'Welcome');
 
-$query = select * from userdata;
-$result = mysqli_query($connect, $query);
+if($connect === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
 
-echo $query;
-echo $result;
 
-echo 1 + 1;
 
+
+$query = "SELECT * FROM userdata";
+
+
+if ($result = mysqli_query($connect, $query)){
+	if(mysqli_num_rows($result) > 0){
+		echo "<table>";
+		 echo "<tr>";
+		  echo "<th>name</th>";
+		  echo "<th>username</th>";
+		  echo "<th>passsword</th>";
+		 echo "</tr>";
+	while ($row = mysqli_fetch_array($result)) {
+		 echo "<tr>";
+		 echo "<td>" . $row['name'] . "</td>";
+		 echo "<td>" . $row['username'] . "</td>";
+		 echo "<td>" . $row['passsword'] . "</td>";
+		 echo "</tr>";
+	} 
+        echo "</table>";
+       
+
+	}else {
+		echo "No records matching your query found";
+	}
+}else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($connect);
+}
+mysqli_close($connect);
 
  ?>
